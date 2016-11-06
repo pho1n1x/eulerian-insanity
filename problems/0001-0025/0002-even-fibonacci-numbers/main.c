cadacff5c9a1b1e7bb9bb731920cdd46367d3f2f
Chris Voelkel <voelkel.chris@googlemail.com>
2016-11-07 09:29:43 +1300

    Add solution in C for Problem 2
    Instead of using a recursive approach for generating the
    Fibonacci sequence, I used Binet's Formula to calculate
    the n-th element of the Fibonacci sequence.

---
+++ problems/0001-0025/0002-even-fibonacci-numbers/main.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <math.h>
+
+#define LIMIT 4000000
+
+// Using Binet's Formula as a closed-form solution
+// to calculate the n-th element of the Fibinacci sequence
+unsigned int binets_formula(int num) {
+    double PHI = (1.0 + sqrt(5)) / 2.0;
+    unsigned int fibonacci_number = 0;
+
+    fibonacci_number = (pow(PHI, num) - pow(1 - PHI, num)) / sqrt(5);
+
+    return fibonacci_number;
+}
+
+int main(void) {
+
+    unsigned int i = 2,
+                 nth_fib_element = 0,
+                 even_fib_sum = 0;
+
+    while ( nth_fib_element < LIMIT ) {
+        nth_fib_element = binets_formula(i);
+        if ( nth_fib_element % 2 == 0 && nth_fib_element < LIMIT ) {
+            even_fib_sum += nth_fib_element;
+        }
+        i++;
+    }
+
+    printf("%d", even_fib_sum);
+
+    return 0;
+}
