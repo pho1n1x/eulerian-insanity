38be70e630bf3468bf30938daa636f5c13ae52e7
Chris Voelkel <voelkel.chris@googlemail.com>
2016-11-09 14:24:36 +1300

    Refactor solution in C for Problems 1 and 2
    Changed the indentation from 4 spaces to 2 spaces. I think that
    2 spaces are sufficient enough for the indentation.
    
    I now use CamelCase for the naming of functions.
    
    Split the functions in function declaration at the beginning
    of the file and the function definition after the `main` function.

--- problems/0001-0025/0001-multiples-of-3-and-5/main.c
+++ problems/0001-0025/0001-multiples-of-3-and-5/main.c
@@ -2,43 +2,47 @@
 
 #define RANGE 1000
 
-unsigned short count_divisibility(unsigned short divisor) {
-    int count = 0;
-
-    count = (RANGE - 1) / divisor;
-
-    return count;
-}
-
-// Use the identity n(n+1)/2 to calculate the sum
-unsigned int calculate_divisibility_sum(unsigned short number, unsigned short count) {
-    unsigned int divisibility_sum = 0;
-
-    divisibility_sum = number * (count * (count + 1)) / 2;
-
-    return divisibility_sum;
-}
+unsigned short CountDivisibility(unsigned short divisor);
+unsigned int CalculateDivisibilitySum(unsigned short num, unsigned short cnt);
 
 int main() {
-    unsigned short div_three_count = 0,
-                   div_five_count = 0,
-                   div_fifteen_count = 0;
+  unsigned short div_three_count = 0,
+                 div_five_count = 0,
+                 div_fifteen_count = 0;
 
-    unsigned int div_three_sum,
-                 div_five_sum,
-                 div_fifteen_sum,
-                 total_sum;
+  unsigned int div_three_sum,
+               div_five_sum,
+               div_fifteen_sum,
+               total_sum;
 
-    div_three_count = count_divisibility(3);
-    div_five_count = count_divisibility(5);
-    div_fifteen_count = count_divisibility(15);
+  div_three_count = CountDivisibility(3);
+  div_five_count = CountDivisibility(5);
+  div_fifteen_count = CountDivisibility(15);
 
-    div_three_sum = calculate_divisibility_sum(3, div_three_count);
-    div_five_sum = calculate_divisibility_sum(5, div_five_count);
-    div_fifteen_sum = calculate_divisibility_sum(15, div_fifteen_count);
+  div_three_sum = CalculateDivisibilitySum(3, div_three_count);
+  div_five_sum = CalculateDivisibilitySum(5, div_five_count);
+  div_fifteen_sum = CalculateDivisibilitySum(15, div_fifteen_count);
 
-    total_sum = div_three_sum + div_five_sum - div_fifteen_sum;
+  total_sum = div_three_sum + div_five_sum - div_fifteen_sum;
 
-    printf("%d", total_sum);
-    return 0;
+  printf("%d", total_sum);
+
+  return 0;
+}
+
+unsigned short CountDivisibility(unsigned short divisor) {
+  unsigned short cnt = 0;
+
+  cnt = (RANGE - 1) / divisor;
+
+  return cnt;
+}
+
+/* Use the identity n(n+1)/2 to calculate the sum */
+unsigned int CalculateDivisibilitySum(unsigned short num, unsigned short cnt) {
+  unsigned int divisibility_sum = 0;
+
+  divisibility_sum = num * (cnt * (cnt + 1)) / 2;
+
+  return divisibility_sum;
 }
